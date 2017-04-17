1f3a6358f937e76b01e5292467b414851c03c1c9
Amy Yates <Amy_SwansonYates@intuit.com>
2017-04-17 11:18:03 -0700

    Initial Commit

---
+++ mysqlstuff/src/main/java/com/mysqlstuff/RunIt.java
@@ -0,0 +1,11 @@
+package com.mysqlstuff;
+
+public class RunIt {
+
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        App dao = new App();
+        dao.readDataBase();
+    }
+
+}
