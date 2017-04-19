20286ac8465b9949f1bf95befd2e993c722bf85b
Amy Yates <Amy_SwansonYates@intuit.com>
2017-04-19 14:54:33 -0700

    Example

--- mysqlstuff/src/main/java/com/mysqlstuff/RunIt.java
+++ mysqlstuff/src/main/java/com/mysqlstuff/RunIt.java
@@ -3,7 +3,6 @@ package com.mysqlstuff;
 public class RunIt {
 
     public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub
         App dao = new App();
         dao.readDataBase();
     }
